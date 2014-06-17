d974b0cc752f711e143669116c1ac8d771ea0b61
Garrett Serack <gserack@gmail.com>
2014-06-17 11:57:09 -0700

    tweaked zip script

--- ZipUpPackage.ps1
+++ ZipUpPackage.ps1
@@ -20,39 +20,56 @@ erase "$OD\*.zip"
 # $n = [int] (([System.DateTime]::Now - [System.DateTime]::Parse("03/19/2014") ).Ticks / 80000000)
 # $n = "$OD\OneGet[#$n].zip" 
 
-$f = "OneGet[$COMMITID].zip"
+$f = "OneGet-build-$COMMITID.zip"
 $n = "$OD\$f" 
 
+copy ".\release-notes.md" $OD
+copy ".\readme.md"  $OD
+
+pushd $OD
 zip "$n" `
-"$OD\Microsoft.OneGet.dll" `
-"$OD\Microsoft.OneGet.MetaProvider.PowerShell.dll" `
-"$OD\Microsoft.OneGet.ServicesProvider.Common.dll" `
-"$OD\Microsoft.OneGet.Utility.dll" `
-"$OD\Microsoft.OneGet.Utility.PowerShell.dll" `
-"$OD\Microsoft.PowerShell.OneGet.dll" `
-"$OD\OneGet.PackageProvider.NuGet.dll" `
-"$OD\Microsoft.OneGet.pdb" `
-"$OD\Microsoft.OneGet.MetaProvider.PowerShell.pdb" `
-"$OD\Microsoft.OneGet.ServicesProvider.Common.pdb" `
-"$OD\Microsoft.OneGet.Utility.pdb" `
-"$OD\Microsoft.OneGet.Utility.PowerShell.pdb" `
-"$OD\Microsoft.PowerShell.OneGet.pdb" `
-"$OD\OneGet.PackageProvider.NuGet.pdb" `
-"$OD\OneGet.format.ps1xml" `
-"$OD\OneGet.psd1" `
-"$OD\TestChainingPackageProvider.psm1" `
-"$OD\TestPackageProvider.psm1" `
-"$OD\nuget.exe" `
-"$OD\etc\*" 
+"Microsoft.OneGet.dll" `
+"Microsoft.OneGet.MetaProvider.PowerShell.dll" `
+"Microsoft.OneGet.ServicesProvider.Common.dll" `
+"Microsoft.OneGet.Utility.dll" `
+"Microsoft.OneGet.Utility.PowerShell.dll" `
+"Microsoft.PowerShell.OneGet.dll" `
+"OneGet.PackageProvider.NuGet.dll" `
+"Microsoft.OneGet.pdb" `
+"Microsoft.OneGet.MetaProvider.PowerShell.pdb" `
+"Microsoft.OneGet.ServicesProvider.Common.pdb" `
+"Microsoft.OneGet.Utility.pdb" `
+"Microsoft.OneGet.Utility.PowerShell.pdb" `
+"Microsoft.PowerShell.OneGet.pdb" `
+"OneGet.PackageProvider.NuGet.pdb" `
+"OneGet.format.ps1xml" `
+"OneGet.psd1" `
+"TestChainingPackageProvider.psm1" `
+"TestPackageProvider.psm1" `
+"nuget.exe" `
+"etc\*" `
+".\release-notes.md" `
+".\readme.md" 
 
 echo ""
 echo ""
 echo "Binary build:"
 echo "    $n"
 
+popd
+
+# push up the build to the download server
 if ($env:COMPUTERNAME -eq "gs-pc" ) {
     ipmo coapp
-    copy-itemex $n coapp:\files
-    echo "build can be downloaded from "
+    pushd $OD
+    copy-item $f oneget-latest.zip 
+    
+    copy-itemex -force $f coapp:\files
+    copy-itemex -force oneget-latest.zip coapp:\files
+    
+    echo "This build can be downloaded from "
     echo "    http://downloads.coapp.org/files/$f"
+    echo "or the latest is always at "
+    echo "    http://downloads.coapp.org/files/oneget-latest.zip"
+    popd
 }
