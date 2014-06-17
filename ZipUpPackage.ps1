49628ed6287cf1bba842f89779e0235f5a8300a2
Garrett Serack <gserack@gmail.com>
2014-06-17 09:47:07 -0700

    added zipuppackage script

---
+++ ZipUpPackage.ps1
@@ -0,0 +1,38 @@
+
+$COMMITID = (git rev-list --full-history --all --abbrev-commit | head -1)
+
+# package script
+$OD = ( resolve-path .\output\v451\AnyCPU\Debug\bin\ ).Path
+
+
+erase "$OD\*.zip"
+# $n = [int] (([System.DateTime]::Now - [System.DateTime]::Parse("03/19/2014") ).Ticks / 80000000)
+# $n = "$OD\OneGet[#$n].zip" 
+
+$n = "$OD\OneGet[$COMMITID].zip" 
+
+zip "$n" `
+"$OD\Microsoft.OneGet.dll" `
+"$OD\Microsoft.OneGet.MetaProvider.PowerShell.dll" `
+"$OD\Microsoft.OneGet.ServicesProvider.Common.dll" `
+"$OD\Microsoft.OneGet.Utility.dll" `
+"$OD\Microsoft.OneGet.Utility.PowerShell.dll" `
+"$OD\Microsoft.PowerShell.OneGet.dll" `
+"$OD\OneGet.PackageProvider.NuGet.dll" `
+"$OD\Microsoft.OneGet.pdb" `
+"$OD\Microsoft.OneGet.MetaProvider.PowerShell.pdb" `
+"$OD\Microsoft.OneGet.ServicesProvider.Common.pdb" `
+"$OD\Microsoft.OneGet.Utility.pdb" `
+"$OD\Microsoft.OneGet.Utility.PowerShell.pdb" `
+"$OD\Microsoft.PowerShell.OneGet.pdb" `
+"$OD\OneGet.PackageProvider.NuGet.pdb" `
+"$OD\OneGet.format.ps1xml" `
+"$OD\OneGet.psd1" `
+"$OD\TestChainingPackageProvider.psm1" `
+"$OD\TestPackageProvider.psm1" `
+"$OD\nuget.exe" `
+"$OD\etc\*" 
+
+
+echo $n
+
