06ef1997b14e23d7101f8f96633f073477f1582d
Garrett Serack <garretts@microsoft.com>
2015-02-02 19:02:51 -0800

    cleaning out dead files

--- OneGet/UploadBuild.ps1
+++
@@ -1,9 +0,0 @@
-﻿## This just zips up the relavent binaries
-ipmo 'C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.psd1'
-ren Install-OneGet.exe install-oneget.exe
-
-copy-itemex -force install-oneget.exe  oneget:providers\
-copy-itemex -force install-oneget.exe  oneget:providers\Install-OneGet.exe
-
-# send-tweet -Message "Posted new #OneGet *Experimental* build https://oneget.org/$n"
-echo build at https://oneget.org/install-oneget.exe 
