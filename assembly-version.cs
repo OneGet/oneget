2a4e10e349787d9577e928633263012985d90f3d
Garrett Serack <garretts@microsoft.com>
2015-03-23 08:21:35 -0700

    tweaking install

--- assembly-version.cs
+++ assembly-version.cs
@@ -19,5 +19,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.15.69.22059")]
-[assembly: AssemblyFileVersion("1.15.69.22059")]
+[assembly: AssemblyVersion("1.15.79.54082")]
+[assembly: AssemblyFileVersion("1.15.79.54082")]
