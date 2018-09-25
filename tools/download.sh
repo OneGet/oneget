88ab2cf46b72ac47f3c58e6365d7c640a9b414d0
Amber Erickson <americks@microsoft.com>
2018-09-25 10:45:06 -0700

    Remove added ruby installating in download.sh

--- tools/download.sh
+++ tools/download.sh
@@ -51,7 +51,6 @@ case "$OSTYPE" in
         ;;
     darwin*)
         # We don't check for curl as macOS should have a system version
-        echo "I added this... current success code: $?"
         package=powershell-6.0.0-beta.5-osx.10.12-x64.pkg
         ;;
     *)
@@ -89,9 +88,7 @@ case "$OSTYPE" in
         ;;
     darwin*)
         patched=0
-        echo "I added this... current success code 2 is: $?"
         if hash brew 2>/dev/null; then
-            rvm reinstall ruby-2.4.0
             if [[ ! -d $(brew --prefix openssl) ]]; then
                echo "Installing OpenSSL with brew..."
                if ! brew install openssl; then
@@ -112,9 +109,7 @@ case "$OSTYPE" in
         fi
 
         echo "Installing $package with sudo ..."
-        echo "I added this... current success code 3: $?"
         sudo installer -pkg "./$package" -target /
-        echo "I added this... current success code 4: $?"
         if [[ $patched -eq 1 ]]; then
             echo "Patching System.Net.Http for libcurl and OpenSSL..."
             find /usr/local/microsoft/powershell -name System.Net.Http.Native.dylib | xargs sudo install_name_tool -change /usr/lib/libcurl.4.dylib /usr/local/opt/curl/lib/libcurl.4.dylib
@@ -122,7 +117,6 @@ case "$OSTYPE" in
         ;;
 esac
 
-echo "I added this... current success code 5: $?"
 powershell -noprofile -c '"Congratulations! PowerShell is installed at $PSHOME"'
 success=$?
 
