7e954de432ab6ab0147ca9be0e8f342f25d0c1a9
Amber Erickson <americks@microsoft.com>
2019-05-07 12:00:59 -0700

    Update verison

--- Test/Examples/Sample_Install_Package.ps1
+++ Test/Examples/Sample_Install_Package.ps1
@@ -23,7 +23,7 @@ configuration Sample_Install_Package
     )
 
 
-    Import-DscResource -Module PackageManagement -ModuleVersion 1.2.4
+    Import-DscResource -Module PackageManagement -ModuleVersion 1.3.2
 
     Node $NodeName
     {               
--- Test/Examples/Sample_Install_Package_Using_NuGet.ps1
+++ Test/Examples/Sample_Install_Package_Using_NuGet.ps1
@@ -38,7 +38,7 @@ configuration Sample_Install_Package_Using_NuGet
         [string]$InstallationPolicy
     )
 
-    Import-DscResource -Module PackageManagement -ModuleVersion 1.2.4
+    Import-DscResource -Module PackageManagement -ModuleVersion 1.3.2
 
     Node $NodeName
     {
