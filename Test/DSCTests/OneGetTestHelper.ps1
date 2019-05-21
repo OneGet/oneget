846c767284f42e4afcf93366685b54a58e278f50
alerickson <25858831+alerickson@users.noreply.github.com>
2019-05-21 09:32:27 -0700

    Up module version to 1.4 (#455)

--- Test/DSCTests/OneGetTestHelper.ps1
+++ Test/DSCTests/OneGetTestHelper.ps1
@@ -53,7 +53,7 @@ configuration Sample_InstallPester
         [string]$DestinationPath       
     )
 
-    Import-DscResource -Module PackageManagement -ModuleVersion 1.3.2
+    Import-DscResource -Module PackageManagement -ModuleVersion 1.4
 
     Node "localhost"
     {
